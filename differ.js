466355e29b5513ea5cc80c94d7bfddd14118cb7d
Gal Schlezinger <gal@spitfire.co.il>
2015-01-31 01:50:51 +0200

    Moved the function out + changed tabs to spaces

--- differ.js
+++ differ.js
@@ -1,35 +1,49 @@
 var diff = require('diff');
 
+var REMOVE = "-";
+var SKIP = "~";
+var ADD = "+";
+
+var differenceActions = {};
+
+differenceActions[REMOVE] = function(from, difference, cursor, text) {
+  cursor += parseInt(difference);
+  return [cursor, text];
+};
+
+differenceActions[SKIP] = function(from, difference, cursor, text) {
+  difference = parseInt(difference, 0);
+  text += from.substr(cursor, difference);
+  cursor += difference;
+  return [cursor, text];
+};
+
+differenceActions[ADD] = function(from, difference, cursor, text) {
+  text = text + difference;
+  return [cursor, text];
+};
+
 module.exports = {
-	createPatch: function(from, to) {
-		return diff.diffWordsWithSpace(from, to).reduce(function(differencesArray, difference, index) {
-			if (difference.removed) {
-				return differencesArray.concat("-" + difference.value.length);
-			} else if (difference.added) {
-				return differencesArray.concat("+" + difference.value);
-			} else {
-				return differencesArray.concat("~" + difference.value.length);
-			}
-		}, []);
-	},
-	applyPatch: function(from, patchJson) {
-		var cursor = 0;
+  createPatch: function(from, to) {
+    return diff.diffWordsWithSpace(from, to).reduce(function(differencesArray, difference, index) {
+      if (difference.removed) {
+        return differencesArray.concat(REMOVE + difference.value.length);
+      } else if (difference.added) {
+        return differencesArray.concat(ADD + difference.value);
+      } else {
+        return differencesArray.concat(SKIP + difference.value.length);
+      }
+    }, []);
+  },
+  applyPatch: function(from, patchJson) {
+    var cursor = 0;
 
-		return patchJson.reduce(function(text, difference) {
-			var differenceAction = difference[0];
-			difference = difference.substr(1);
-
-			if (differenceAction === "~") {
-				difference = parseInt(difference, 0);
-				text += from.substr(cursor, difference);
-				cursor += difference;
-				return text;
-			} else if (differenceAction === "+") {
-				return text + difference;
-			} else if (differenceAction === "-") {
-				cursor += parseInt(difference, 0);
-				return text;
-			}
-		}, "");
-	}
+    return patchJson.reduce(function(text, difference) {
+      var differenceAction = difference[0];
+      difference = difference.substr(1);
+      var results = differenceActions[differenceAction](from, difference, cursor, text);
+      cursor = results[0];
+      return results[1];
+    }, "");
+  }
 };
